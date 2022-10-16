36286df4e07d2f3e27172c57c37e30cbcaea6cc4
王天赐 <15565946702@163.com>
2022-10-16 20:41:02 +0800

    王天赐, feat: 添加单调栈模板

---
+++ leetcode-codes-template/src/main/java/cn/knightzz/template/stack/MonotonousStackTemplate.java
@@ -0,0 +1,14 @@
+package cn.knightzz.template.stack;
+
+/**
+ * @author 王天赐
+ * @title: MonotonousStackTemplate
+ * @projectName algorithm-codes
+ * @description: 单调栈模板
+ * @website <a href="http://knightzz.cn/">http://knightzz.cn/</a>
+ * @github <a href="https://github.com/knightzz1998">https://github.com/knightzz1998</a>
+ * @create: 2022-10-16 20:40
+ */
+public class MonotonousStackTemplate {
+
+}
---
+++ leetcode-codes-template/src/main/java/cn/knightzz/template/stack/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * 存放单调栈模板
+ */
+package cn.knightzz.template.stack;
